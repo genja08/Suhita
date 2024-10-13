ad271330a350316c29632b343f08b00abf29b504
Ihtiandiko03 <wihtiandiko@gmail.com>
2024-10-13 10:11:25 +0700

    u

--- resources/js/Components/Navbar/Navbar.jsx
+++ resources/js/Components/Navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import './Navbar.css';
 const Navbar = () => {
   const [scrolled, setScrolled] = useState(false);
   const [activeLink, setActiveLink] = useState('');
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const handleScroll = () => {
     const offset = window.scrollY;
@@ -12,13 +13,11 @@ const Navbar = () => {
 
   const updateActiveLink = () => {
     const currentPath = window.location.pathname;
-    setActiveLink(currentPath); // Update the active link state with the current path
+    setActiveLink(currentPath);
   };
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
-
-    // Call the function to detect the current active link
     updateActiveLink();
 
     return () => {
@@ -26,21 +25,21 @@ const Navbar = () => {
     };
   }, []);
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <nav className={navbar ${scrolled ? 'scrolled' : ''}}>
-      <img 
-        src="/img/logo.png" 
-        alt="Suhita Logo" 
-        className='logo'
-      />
-      <div className='nav'>
+    <nav className={`navbar ${scrolled ? 'scrolled' : ''}`}>
+      <img src="/img/logo.png" alt="Suhita Logo" className="logo" />
+      <div className={`nav ${menuOpen ? 'open' : ''}`}>
         <a href="/" className={activeLink === '/' ? 'active' : ''}>BERANDA</a>
         <a href="/tentang" className={activeLink === '/tentang' ? 'active' : ''}>TENTANG KAMI</a>
         <a href="/produk" className={activeLink === '/produk' ? 'active' : ''}>PRODUK</a>
         <a href="/kontak" className={activeLink === '/kontak' ? 'active' : ''}>KONTAK</a>
       </div>
-      <div className='searchBar'>
-        {/* Search bar component if needed */}
+      <div className="hamburger" onClick={toggleMenu}>
+        &#9776; {/* Hamburger icon */}
       </div>
     </nav>
   );
